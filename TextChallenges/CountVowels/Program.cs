758886e340b511dec530b0ec83c489e74004aa83
Andrew Tsai <andrewtsai1220@hotmail.com>
2016-04-11 23:16:37 -0600

    created CountVowels project.

---
+++ TextChallenges/CountVowels/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountVowels
+{
+    /// <summary>
+    /// Counts the number of vowels in a word or text file
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
